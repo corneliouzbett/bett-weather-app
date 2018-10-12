702805bffe21fdf492ba9c321961e22c3e57a6f9
corneliouzBett <kbett68@gmail.com>
2018-10-12 10:50:39 +0300

    Refactored Weather service (added comments)

--- src/app/service/weather.service.ts
+++ src/app/service/weather.service.ts
@@ -5,6 +5,10 @@ import {HttpClient} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
+/*
+* weather service
+* @author corneliouzBett
+* */
 export class WeatherService {
 
   private BASEURL = 'http://api.openweathermap.org/data/2.5';
@@ -13,6 +17,10 @@ export class WeatherService {
   constructor(private http: HttpClient) {
   }
 
+  /*
+  * takes city(string) as the parameter
+  * returns current weather of a city at that time
+  * */
   getCurrentWeatherOfaCity(cityName: String): Observable<any> {
     return this.http.get(`${this.BASEURL}/weather?q=${cityName}&APPID=${this.APP_ID}`);
   }
